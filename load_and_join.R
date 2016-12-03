2cb408cda5c9d7bfd6f91f66be6eaf212b56d34f
fmark <francis.markham@anu.edu.au>
2016-12-03 13:23:02 +1100

    Initial commit.

---
+++ load_and_join.R
@@ -0,0 +1,5 @@
+library(dplyr)
+
+us_county_data <- read.csv("https://raw.githubusercontent.com/tonmcg/County_Level_Election_Results_12-16/master/US_County_Level_Presidential_Results_12-16.csv")
+
+
